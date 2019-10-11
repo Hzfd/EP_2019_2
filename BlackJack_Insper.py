2c0e92b1fa21ebe818e6fe6c06e4b1a42e766664
Hzfd <31143688+Hzfd@users.noreply.github.com>
2019-10-11 14:15:35 -0300

    variaveis para o loop criadas

---
+++ BlackJack_Insper.py
@@ -0,0 +1,11 @@
+#BlackJack_Insper
+import random
+print ("Bem vindo ao Blackjack Insper!")
+
+deck=[2,3,4,5,6,7,8,9,10,11,12,13,14]*4
+cartas = {"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"11":10,"12":10,"13":10,"14":1}
+carteira = float(input("Com quanto dinheiro deseja entrar no jogo? Digite o valor: "))
+aposta = 0
+
+
+        
