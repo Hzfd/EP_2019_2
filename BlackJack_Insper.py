6bd7290b8c4e356f9211dfb14d9d5ffb79aed8d3
Hzfd <31143688+Hzfd@users.noreply.github.com>
2019-10-14 17:52:43 -0300

    jogo aparentemente funcional com a feature 1 e 2 funcionando,feature 3 em fase de teste

--- BlackJack_Insper.py
+++ BlackJack_Insper.py
@@ -8,15 +8,32 @@ cartas = {"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"11":10,"12":1
 
 
 #Iniciando o jogo
-recebe_valores=[]
-recebe_valores_pc = []
 
+game = True
 entrar=input("Este é o BlackJack Insper!!! Deseja entrar na mesa? (s/n): ")
 carteira = float(input("Com quanto dinheiro deseja entrar no jogo? Digite o valor: "))
 if entrar=="s":
+    rodada = 1
 
-    while carteira>=0:
+    while game == True:
+
+        recebe_valores=[]
+        recebe_valores_pc = []
+
+        print("rodada:{0}".format(rodada))
+        aposta_l=[]
         aposta = float(input("Digite o quanto quer apostar: "))
+
+        if str(aposta)=="fim":
+            print("Foi bom jogar com você, volte sempre!!!")
+            break
+
+        #Feature 1
+        while aposta>carteira:
+            print("Você não pode apostar mais dinheiro do que tem, aposte um valor adequado")
+            aposta = float(input("Digite o quanto quer apostar: "))
+
+        aposta_l.append(aposta)
         carteira-=aposta
         print("Carteira: ",carteira)
 
@@ -41,50 +58,124 @@ if entrar=="s":
         recebe_valores_pc.append(valor_2_pc)
         print (recebe_valores)
         
-        if sum(recebe_valores) and sum(recebe_valores_pc) == 21:
-            print("empate")
+        if sum(recebe_valores)==21 and sum(recebe_valores_pc) == 21:
+            print("Você empatou com a IA, mais sorte na proxima tentativa!!")
+            print("Carteira: {0}".format(carteira))
+            print("NOVA RODADA")
 
-        elif sum(recebe_valores)==21:
-            ganho = aposta + 1.5*aposta
-            carteira+=ganho
-            print("Sua soma: ",sum(recebe_valores))
-            print("Soma pc: ",sum(recebe_valores_pc))
-            print("Você fez BlackJack, PARABENS!!!!, você ganhou {0}".format(ganho),"agora você tem: R${0}".format(carteira))
-            break
+        elif sum(recebe_valores_pc)>21:
+            print("A IA estourou, você ganhou!!!")
+            carteira+=sum(aposta_l)+1.5*sum(aposta_l)
+            print("Carteira {0}".format(carteira))
+
+        elif sum(recebe_valores)>21 and sum(recebe_valores_pc)>21:
+            print("Você e a IA estouraram, EMPATE!!!")
+            carteira+=sum(aposta)
+            print("Carteira{0}".format(carteira))
 
         elif sum(recebe_valores_pc)==21:
-            ganho = aposta 
+            ganho = sum(aposta_l) 
             carteira-=ganho
             print("Sua soma: ",sum(recebe_valores))
             print("Soma pc: ",sum(recebe_valores_pc))
             print("O IA ganhou de você!! Ele alcançou os 21 pontos! Tente novamente")
             print("agora você tem: R${0}".format(carteira))
-            break
+            
 
-        elif sum(recebe_valores) < 21:
-            p = str(input("Você não fez um Blackjack, deseja parar?(sim/nao): "))
-                
-            if p=="sim":
-                if sum(recebe_valores)>sum(recebe_valores_pc):
-                    ganho = 2*aposta
-                    carteira+=ganho
-                    print ("Parabens, voce ganhou a rodada! Voce ganhou R${0}!! Agora possui uma carteira de R${1}".format(ganho,carteira))
-                    break
-                elif sum(recebe_valores)<sum(recebe_valores_pc):
-                    carteira-=aposta
-                    print ("Voce perdeu essa rodada...!! Agora você possui uma carteira de R${0}".format(carteira))
-                    break
-            else:
-                print ("rodada: ")
-
+        elif sum(recebe_valores)==21:
+            ganho = sum(aposta_l) + 1.5*sum(aposta_l)
+            carteira+=ganho
+            print("Sua soma: ",sum(recebe_valores))
+            print("Soma IA: ",sum(recebe_valores_pc))
+            print("Você fez BlackJack, PARABENS!!!!, você ganhou {0}".format(ganho),"agora você tem: R${0}".format(carteira))
+        
         elif sum(recebe_valores)>21:
             print("Sua soma: ",sum(recebe_valores))
             print("Soma pc: ",sum(recebe_valores_pc))
             print("Você perdeu! Estourou os 21 pontos, perdeu R${0}".format(aposta))
-            carteira-=aposta
+            carteira-=sum(aposta_l)
             print("Carteira",carteira)
-            break
-     
+  
+
+        elif sum(recebe_valores) < 21:
+            p = str(input("Você não fez um Blackjack, deseja parar?(sim/nao): "))
+                
+            if p=="nao":
+                if sum(recebe_valores)>17 or sum(recebe_valores_pc)>17:
+                    if sum(recebe_valores)>sum(recebe_valores_pc):
+                        print("Você chegou mais proximo de um BlackJack que a IA, ganhou essa rodada!!!")
+                        ganho = aposta*2
+                        carteira+=ganho
+                        print("Carteira: R${0}".format(carteira))
+                    elif sum(recebe_valores_pc)>sum(recebe_valores):
+                        print("Perdeu!!! A IA chegou mais proximo de um BlackJack que você, mais sorte na proxima")
+                        print("Carteira: R${0}".format(carteira))
+                else:
+                    while sum(recebe_valores)<=17 and sum(recebe_valores_pc)<=17:
+
+                        aposta = float(input("Digite o quanto quer apostar: "))         
+                        #Feature 1
+                        while aposta>carteira:
+                            print("Você não pode apostar mais dinheiro do que tem, aposte um valor adequado")
+                            aposta = float(input("Digite o quanto quer apostar: "))
+
+                        aposta_l.append(aposta)
+                        carteira-=aposta
+                        print("Carteira: R${0} ".format(carteira))
+
+                        primeira_carta = random.randint(2,14)
+                        primeira_carta_pc = random.randint(2,14)
+                        
+
+                        segunda_carta = random.randint(2,14)
+                        segunda_carta_pc = random.randint(2,14)
+                    
+
+                        valor_1 = cartas["{0}".format(primeira_carta)]
+                        recebe_valores.append(valor_1)
+
+                        valor_1_pc = cartas["{0}".format(primeira_carta_pc)]
+                        recebe_valores_pc.append(valor_1_pc)
+
+                        valor_2 = cartas["{0}".format(segunda_carta)]
+                        recebe_valores.append(valor_1)
+
+                        valor_2_pc = cartas["{0}".format(segunda_carta_pc)]
+                        recebe_valores_pc.append(valor_2_pc)
+                        print (recebe_valores)
+
+                        if sum(recebe_valores)>17 or sum(recebe_valores_pc)>17:
+
+                            if sum(recebe_valores)>sum(recebe_valores_pc):
+                                ganho = 2*sum(aposta_l)
+                                carteira+=ganho
+                                print("Você chegou mais proxímo de um BlackJack que a IA, ganhou!!!")
+                                print("Carteira:{0}".format(carteira))
+                                
+
+                            elif sum(recebe_valores)<sum(recebe_valores_pc):
+                                carteira-=sum(aposta_l)
+                                print ("Voce perdeu essa rodada...!! Agora você possui uma carteira de R${0}".format(carteira))
+                            
+            elif p=="sim":
+                if sum(recebe_valores)>sum(recebe_valores_pc):
+                    print("Você chegou mais próximo que a IA, ganhou!!!")
+                    ganho =  sum(aposta_l)
+                    carteira+=ganho
+                    print("Carteira: R${0}".format(carteira))
+
+                elif sum(recebe_valores_pc)>sum(recebe_valores):
+                    print("Perdeu!!! A IA chegou mais proximo de um BlackJack que você, mais sorte na próxima vez")
+                    print("Carteira: R${0}".format(carteira))
+
+        
+            
+
+        #Feature 2
+        elif carteira<=0:
+            game = False
+        
+        rodada+=1
 
 else:
     print("Volte quando quiser jogar!!")
